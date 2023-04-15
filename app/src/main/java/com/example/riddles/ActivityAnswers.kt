65bef91343d0dc0e0951d1548eafea2ea5329cd5
HUAWEi <HUAWEi@SMIRNOVAPC>
2023-04-15 13:52:38 +0300

    .

--- app/src/main/java/com/example/riddles/ActivityAnswers.kt
+++ app/src/main/java/com/example/riddles/ActivityAnswers.kt
@@ -2,67 +2,58 @@ package com.example.riddles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.riddles.databinding.ActivityAnswersBinding
 
 class ActivityAnswers : AppCompatActivity() {
     private lateinit var binding: ActivityAnswersBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnswersBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Name()
+        binding.btnAnswering.setOnClickListener {
+            Poisk()
+        }
+    }
+    var namezag = ""
+    var RadBtn = listOf("Ландыш","Поезд","Яблоня","Ромашка","Медуза","Белка","Снег","Карта","Ёлка","Береза")
+    var RadBtnRand = RadBtn.shuffled()
+    fun Name() {
+        binding.radioButton1.text = RadBtnRand[0]
+        binding.radioButton2.text = RadBtnRand[1]
+        binding.radioButton3.text = RadBtnRand[2]
+        binding.radioButton4.text = RadBtnRand[3]
+        binding.radioButton5.text = RadBtnRand[4]
+        binding.radioButton6.text = RadBtnRand[5]
+        binding.radioButton7.text = RadBtnRand[6]
+        binding.radioButton8.text = RadBtnRand[7]
+        binding.radioButton9.text = RadBtnRand[8]
+        binding.radioButton10.text = RadBtnRand[9]
+    }
+    fun Poisk() {
+        when (binding.RadioGroup.checkedRadioButtonId) {
+            binding.radioButton1.id -> namezag = binding.radioButton1.text.toString()
+            binding.radioButton2.id -> namezag = binding.radioButton2.text.toString()
+            binding.radioButton3.id -> namezag = binding.radioButton3.text.toString()
+            binding.radioButton4.id -> namezag = binding.radioButton4.text.toString()
+            binding.radioButton5.id -> namezag = binding.radioButton5.text.toString()
+            binding.radioButton6.id -> namezag = binding.radioButton6.text.toString()
+            binding.radioButton7.id -> namezag = binding.radioButton7.text.toString()
+            binding.radioButton8.id -> namezag = binding.radioButton8.text.toString()
+            binding.radioButton9.id -> namezag = binding.radioButton9.text.toString()
+            binding.radioButton10.id -> namezag = binding.radioButton10.text.toString()
+        }
 
-        binding.radioButton1.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton1.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton2.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton2.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton3.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton3.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton4.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton4.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton5.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton5.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton6.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton6.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton7.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton7.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton8.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton8.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton9.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton9.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.radioButton10.setOnClickListener{
-            binding.btnAnswering.isEnabled=true
-            intent.putExtra("answer", binding.radioButton10.text.toString())
-            setResult(RESULT_OK,intent)
-        }
-        binding.btnAnswering.setOnClickListener{
+        if (namezag != "") {
+            var idOtv = RadBtn.indexOf(namezag)
+            intent.putExtra("put", idOtv.toString())
+            intent.putExtra("Rad", namezag)
+            setResult(RESULT_OK, intent)
             finish()
         }
     }
 }
+
+
--- app/src/main/java/com/example/riddles/MainActivity.kt
+++ app/src/main/java/com/example/riddles/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,132 +14,103 @@ import com.example.riddles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var riddles2 = 0
-    var riddles3 = mutableListOf<Int>()
-    private var launcher: ActivityResultLauncher<Intent>? = null
-    @SuppressLint("SetTextI18n")
+    private var trueCounter = 0
+    private var falseCounter = 0
+    private var allCounter = 0
+    private var i=0
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var answerConst = ""
-        var trueCounter = 0
-        var falseCounter = 0
-        var allCounter = 0
-        binding.btnStatistic.setOnClickListener{
-            val intent = Intent(this, ActivityStatistics::class.java)
-            intent.putExtra("trueCount", trueCounter.toString())
-            intent.putExtra("falseCount", falseCounter.toString())
-            intent.putExtra("allCount", allCounter.toString())
-            startActivity(intent)
-        }
 
-        launcher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
-                if (result.resultCode == RESULT_OK){
-                    val answerFromUser = result.data?.getStringExtra("answer")
-                    binding.tvAnswer.text = "$answerFromUser"
-                    binding.btnGetRiddle.isEnabled = true
-                    if (answerFromUser == answerConst)
-                    {
-                        binding.tvAnswer.setBackgroundColor(Color.GREEN)
-                        ++trueCounter
-                    }
-                    else {
-                        binding.tvAnswer.setBackgroundColor(Color.RED)
-                        ++falseCounter
-                    }
-                    if (allCounter == 10)
-                    {
-                        binding.btnStatistic.isVisible = true
-                        binding.btnGetRiddle.isVisible = false
-                        binding.btnGiveAnswer.isVisible = false
-                        binding.btnRepeat.isVisible = true
-                        binding.btnExit.isVisible = true
-
-                    }
-                }
+            binding.btnStatistic.setOnClickListener {
+                intent = Intent(this, ActivityStatistics::class.java)
+                intent.putExtra("trueCount", trueCounter.toString())
+                intent.putExtra("falseCount", falseCounter.toString())
+                intent.putExtra("allCount", allCounter.toString())
+                startActivity(intent)
             }
 
-        binding.btnGetRiddle.setOnClickListener{
-            binding.tvAnswer.text=""
-            binding.btnGiveAnswer.isEnabled = true
-            binding.btnGetRiddle.isEnabled = false
-            ++allCounter
-            binding.tvRiddlesCounterBase.text = allCounter.toString()
+            binding.btnGetRiddle.setOnClickListener {
+                binding.tvAnswer.text = ""
+                binding.btnGiveAnswer.isEnabled = true
+                binding.btnGetRiddle.isEnabled = false
+                ++allCounter
+                binding.tvRiddles.text="${Clone[i]}"
+                binding.tvAnswer.setBackgroundColor(Color.WHITE)
+                i++
+                binding.tvRiddlesCounterBase.text = allCounter.toString()
+            }
 
-            binding.btnRepeat.setOnClickListener{
+            binding.btnRepeat.setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
 
-            binding.btnExit.setOnClickListener{
+            binding.btnExit.setOnClickListener {
                 finish()
             }
-
-            while (true)
+            binding.btnGiveAnswer.setOnClickListener {
+                intent = Intent(this, ActivityAnswers::class.java)
+                startActivityForResult(intent,100)
+                binding.btnGiveAnswer.isEnabled = false
+                binding.btnGetRiddle.isEnabled = true
+            }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==100&&resultCode== RESULT_OK&&data!=null) {
+            val indexMas= RiddlesArr.indexOf(Clone[i-1])
+            if (indexMas.toString()==data.getStringExtra("put")) {
+                binding.tvAnswer.setBackgroundColor(Color.GREEN)
+                binding.tvAnswer.text=data.getStringExtra("Rad")
+                ++trueCounter
+            }
+            if(indexMas.toString()!=data.getStringExtra("put"))
             {
-                riddles2 = (1..10).random()
-                if (riddles2 !in riddles3)
-                {
-                    riddles3.add(riddles2)
-                    break
-                }
+                binding.tvAnswer.setBackgroundColor(Color.RED)
+                binding.tvAnswer.text=data.getStringExtra("Rad")
+                ++falseCounter
             }
+            if (allCounter == 10) {
+                binding.btnStatistic.isVisible = true
+                binding.btnGetRiddle.isVisible = false
+                binding.btnGiveAnswer.isVisible = false
+                binding.btnRepeat.isVisible = true
+                binding.btnExit.isVisible = true
 
-            when (riddles2){
-                1 -> {binding.tvRiddles.text = "Этот цветок распускается в мае, \n" +
-                        "Белые бусинки он одевает."
-                    answerConst = "Ландыш"
-                }
-                2-> {
-                    binding.tvRiddles.text =  "Мчится печка впереди,\n"+
-                            "Тащит избы позади."
-                    answerConst = "Поезд"
-                }
-                3-> {binding.tvRiddles.text = "По весне глаз радует,\n " +
-                        "по осени – язычок."
-                    answerConst = "Яблоня"}
-                4-> {binding.tvRiddles.text = "До чего ж цветок пригож – \n" +
-                        "Он на солнышко похож."
-                    answerConst = "Ромашка"
-                }
-                5-> {
-                    binding.tvRiddles.text = "Зонтик этот ты не тронь –\n" +
-                            "Обжигает, как огонь. "
-                    answerConst="Медуза"
-                }
-                6-> {
-                    binding.tvRiddles.text = "Сидит на ветке, а не птица.\n" +
-                            "Есть рыжый хвост, а не лисица."
-                    answerConst = "Белка"
-                }
-                7-> {
-                    binding.tvRiddles.text = "На всех садится, никого не боится."
-                    answerConst = "Cнег"
-                }
-                8-> {
-                    binding.tvRiddles.text = "Государства и моря –\n" +
-                            "Все на стенке у меня."
-                    answerConst = "Карта"
-                }
-                9-> {
-                    binding.tvRiddles.text = "Дерево-ёж\n" +
-                            "Носит платье клёш."
-                    answerConst = "Ёлка"
-                }
-                10-> {
-                    binding.tvRiddles.text = "Зелена, а не трава;\n" +
-                            "белена, а не снег;\n" +
-                            "кудрява, а без волос."
-                    answerConst = "Береза"
-                }
             }
-        }
-        binding.btnGiveAnswer.setOnClickListener{
-            val intent = Intent(this, ActivityAnswers::class.java)
-            launcher?.launch(intent)
-            binding.btnGiveAnswer.isEnabled = false
         }
     }
+
+    val RiddlesArr = arrayOf(
+        "Этот цветок распускается в мае, \n" +
+                "Белые бусинки он одевает.",
+        "Мчится печка впереди,\n" +
+                "Тащит избы позади.",
+        "По весне глаз радует,\n " +
+                "по осени – язычок.",
+        "До чего ж цветок пригож – \n" +
+                "Он на солнышко похож.",
+        "Зонтик этот ты не тронь –\n" +
+                "Обжигает, как огонь. ",
+        "Сидит на ветке, а не птица.\n" +
+                "Есть рыжый хвост, а не лисица.",
+        "На всех садится, никого не боится.",
+        "Государства и моря –\n" +
+                "Все на стенке у меня.",
+        "Дерево-ёж\n" +
+                "Носит платье клёш.",
+        "Зелена, а не трава;\n" +
+                "белена, а не снег;\n" +
+                "кудрява, а без волос.")
+
+    val Clone= copyArray(RiddlesArr)
+    fun copyArray(source: Array<String>): Array<String?> {
+        return source.copyOf(source.size)
+    }
 }
+
+
+
